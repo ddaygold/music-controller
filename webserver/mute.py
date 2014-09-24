1efae87d2614522b04605582fb50b51465e2e35a
Peter Chinetti <peter@chinetti.me>
2014-09-24 16:12:30 -0500

    working now"

--- webserver/mute.py
+++ webserver/mute.py
@@ -1,16 +1,20 @@
-#!/usr/bin/env python
 import alsaaudio
-output = '<!DOCTYPE html\n\
-PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\n\
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n\
-<html>\n\
-	<head>\n\
-		<title>Muted</title>\n\
-	</head>\n\
-	<body>\n\
-            <p>Muted</p>\n\
-	</body>\n\
-</html>' 
+output = '<!DOCTYPE html\
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\
+<html>\
+<head>\
+    <title>MUTE</title>\
+    <style text="text/javascript">\
+    .full { color: white; background-color: red; width: 100%; height: 100%; text-align: center; font-size: 1500%}\
+    </style>\
+</head>\
+<body>\
+<div class="full">\
+MUTED\
+</div>\
+</html>\
+'
 m = alsaaudio.Mixer(control='Master',id=0,cardindex=0)
 m.setmute(1)
 print(output)
