eccfd0d1b02c09af6a92fc2c9358219cb4270b7b
Peter Chinetti <peter@chinetti.me>
2014-09-24 13:05:32 -0500

    formattng

--- webserver/mute.py
+++ webserver/mute.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 import alsaaudio
-output = '<!DOCTYPE html\
-PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\
-<html>\
-	<head>\
-		<title>Muted</title>\
-	</head>\
-	<body>\
-            <p>Muted</p>\
-	</body>\
+output = '<!DOCTYPE html\n\
+PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"\n\
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">\n\
+<html>\n\
+	<head>\n\
+		<title>Muted</title>\n\
+	</head>\n\
+	<body>\n\
+            <p>Muted</p>\n\
+	</body>\n\
 </html>' 
 m = alsaaudio.Mixer(control='Master',id=0,cardindex=0)
 m.setmute(1)
